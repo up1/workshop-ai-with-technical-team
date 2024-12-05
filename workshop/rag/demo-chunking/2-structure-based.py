55356826a8d367bbec99aae3586b98d82d519af7
up1 <somkiat.p@gmail.com>
2024-12-05 13:36:46 +0700

    Split data

--- workshop/rag/demo-chunking/2-structure-based.py
+++ workshop/rag/demo-chunking/2-structure-based.py
@@ -1,4 +1,5 @@
 from langchain_text_splitters import HTMLHeaderTextSplitter
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 
 # Specify the URL from which the text should be obtained
 url="https://en.wikipedia.org/wiki/FIFA_World_Cup"
@@ -17,3 +18,16 @@ html_splitter = HTMLHeaderTextSplitter(headers_to_split_on=headers_to_split_on)
 # Create splits in text obtained from the url
 html_header_splits = html_splitter.split_text_from_url(url)
 print(html_header_splits)
+
+
+# Specify the chunk size and overlap with RecursiveCharacterTextSplitter
+chunk_size = 500
+chunk_overlap = 30
+text_splitter = RecursiveCharacterTextSplitter(
+    chunk_size=chunk_size, chunk_overlap=chunk_overlap
+)
+
+# Split
+splits = text_splitter.split_documents(html_header_splits)
+print(len(splits))
+print(splits[10])
