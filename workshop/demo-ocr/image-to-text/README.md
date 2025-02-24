e9a63d5280f8f319ddecc97586ab8f92a4524e25
up1 <somkiat.p@gmail.com>
2025-02-24 22:15:10 +0700

    Read text from image

---
+++ workshop/demo-ocr/image-to-text/README.md
@@ -0,0 +1,16 @@
+# Workshop with Image to Text with OpenAI
+
+
+## Solution 1 :: Using OCR (Optical Character Recognition)
+
+
+
+## Solution 2 :: Using OpenAI Model
+* Convert image to base64
+* Send data to OpenAI
+  * data:image/png;base64
+
+```
+$export OPENAI_API_KEY=<your key>
+$python solution_2.py
+```
---
+++ workshop/demo-ocr/image-to-text/solution_2.py
@@ -0,0 +1,56 @@
+from openai import OpenAI
+import base64
+
+client = OpenAI()
+
+def image_to_base64(image_path):
+    with open(image_path, "rb") as image_file:
+        return base64.b64encode(image_file.read()).decode("utf-8")
+
+def image_to_text_from_url(image_url):
+    response = client.chat.completions.create(
+        model="gpt-4o-mini",
+        messages=[
+            {
+                "role": "user",
+                "content": [
+                    {"type": "text", "text": "Extract all the text content from this image."},
+                    {"type": "image_url", "image_url": {"url": image_url}},
+                ],
+            }
+        ],
+        max_tokens=300,
+    )
+    return response.choices[0].message.content
+
+def image_to_text_from_base64(image_base64):
+    response = client.chat.completions.create(
+        model="gpt-4o-mini",
+        messages=[
+            {
+                "role": "user",
+                "content": [
+                    {
+                        "type": "text",
+                        "text": (
+                            "Extract all the text content from this image thoroughly and accurately. Ensure that no lines, words, or parts of the content are missed, even if the text is faint, small, or near the edges. The text may include headings, paragraphs, or lists and could appear in various fonts, styles, or layouts. Carefully preserve the reading order and structure as it appears in the image. Double-check for any skipped lines or incomplete content, and extract every visible text element, ensuring completeness across all sections. This is crucial for the task's accuracy."
+                        )
+                    },
+                    {"type": "image_url", "image_url": {
+                        "url": f"data:image/png;base64,{image_base64}"}
+                    },
+                ],
+            }
+        ],
+        max_tokens=300,
+    )
+    print(response)
+    return response.choices[0].message.content
+
+# Example usage:
+if __name__ == "__main__":
+    local_image_path = "image.png"
+    image_base64 = image_to_base64(local_image_path)
+    print("\nExtracted text from Base64:")
+    extracted_text = image_to_text_from_base64(image_base64)
+    print(extracted_text)
