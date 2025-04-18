4a863f1984af23c88363e75430f0b8841497ecd8
up1 <somkiat.p@gmail.com>
2025-04-18 14:26:48 +0700

    Integrate with openai api + prompt template

--- workshop/demo-caching/demo-semantic-caching/demo-caching.py
+++ workshop/demo-caching/demo-semantic-caching/demo-caching.py
@@ -1,19 +1,30 @@
 from caching import get_from_cache, store_in_cache
+from langchain_openai import ChatOpenAI
+from langchain_core.prompts import PromptTemplate
 
 def get_response(query):
     cached_response = get_from_cache(query)
     if cached_response:
+        print(f"Cache hit for query: {query}")
         return cached_response
     else:
+        print(f"Cache miss for query: {query}")
         # Simulate a call to an LLM
         response = call_llm(query)
         store_in_cache(query, response)
         return response
 
 def call_llm(query):
-    # Simulate a call to an LLM
-    # In a real-world scenario, this would be replaced with actual LLM call
-    return f"Response for query: {query}"
+    llm = ChatOpenAI(model="gpt-4")
+    prompt = PromptTemplate.from_template("As assistant, I will answer the question in {output_language}.\n\n{input}\n\nAnswer:")
+    chain = prompt | llm
+    response = chain.invoke(
+        {
+            "output_language": "thai",
+            "input": query,
+        }
+    )
+    return response.content
 
 if __name__ == "__main__":
     print("========== 1st call ==============")
